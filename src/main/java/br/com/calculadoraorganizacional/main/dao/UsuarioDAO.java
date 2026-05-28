a3ce78814ed1e59480f98beb05cb14a91d38cb71
Jorge_Belo <jorgeamadobelo@hotmail.com>
2026-05-27 21:20:49 -0300

    Implementado cadastro de usuários com MySQL

--- src/main/java/br/com/calculadoraorganizacional/main/dao/UsuarioDAO.java
+++ src/main/java/br/com/calculadoraorganizacional/main/dao/UsuarioDAO.java
@@ -5,6 +5,7 @@ import br.com.calculadoraorganizacional.model.Usuario;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UsuarioDAO {
@@ -35,5 +36,44 @@ public class UsuarioDAO {
             System.out.println("Erro ao cadastrar usuário!");
             e.printStackTrace();
         }
+    }
+
+    public Usuario realizarLogin(String login, String senha) {
+
+        String sql = "SELECT * FROM usuarios WHERE login = ? AND senha = ?";
+
+        try {
+
+            Connection conn = ConexaoMySQL.conectar();
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1, login);
+            stmt.setString(2, senha);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+
+                Usuario usuario = new Usuario();
+
+                usuario.setId(rs.getInt("id"));
+                usuario.setNome(rs.getString("nome"));
+                usuario.setLogin(rs.getString("login"));
+                usuario.setSenha(rs.getString("senha"));
+
+                return usuario;
+            }
+
+            stmt.close();
+            conn.close();
+
+        } catch (SQLException e) {
+
+            System.out.println("Erro ao realizar login!");
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
